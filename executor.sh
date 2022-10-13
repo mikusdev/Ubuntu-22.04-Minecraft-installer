30feb21753a08feedbc236ea1c863da271bae1b9
Mikus <92790899+mikusdev@users.noreply.github.com>
2022-10-13 09:25:16 +0100

    Update executor.sh

--- executor.sh
+++ executor.sh
@@ -1,1 +1,4 @@
-
+echo "Fetching installer"
+wget -O installer.sh https://raw.githubusercontent.com/mikusdev/Ubuntu-22.04-Minecraft-installer/main/installer.sh
+chmod u+x installer.sh
+./installer.sh
